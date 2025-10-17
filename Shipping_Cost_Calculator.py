ccc37b9182bdc634805d93a12eb80584eef59b74
Christian Binay-an <christianbinayan3@gmail.com>
2025-10-17 04:49:06 -0400

    Add new function to calculator

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,13 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+ # Here is a new update by cbins13
+ # Input quantity and unit price
+quantity = int(input("Enter the quantity of items: "))
+unit_price = float(input("Enter the price per item in USD: "))
+
+# Calculate total price
+total_price = quantity * unit_price
+
+# Display the result
+print(f"Total Price: {total_price} USD")
